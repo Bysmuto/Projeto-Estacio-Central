aa37d9ed1ca12dbd5ed5e2d5a3bce5f0f05c0c8f
bys <bysmuto@hotmail.com>
2022-08-17 06:35:37 -0300

    antes de modificar

--- Central.py
+++ Central.py
@@ -14,23 +14,23 @@ dsn = {   'fonte1': 'Franklin 25 bold',
           'laranja2': '#F4D9D7',
           'relif': FLAT,
           'y_dist': 10,
-          'x_dist': 10,         }
+          'x_dist': 20,         }
 
 janela = Tk()
-janela.geometry('550x300+400+150')
+janela.geometry('520x300+400+150')
 janela.config(bg=dsn['branco'])
 
-titulo = Label(janela, text='Central de ferramentas', font=dsn['fonte1'], bg=dsn['branco'], fg=dsn['laranja1'])
+titulo = Label(janela, text='Central Ferramentas', font=dsn['fonte1'], bg=dsn['branco'], fg=dsn['laranja1'])
 titulo.grid(columnspan=2,column=1,row=1,padx=100,pady=40)
 
 b_ferramentas = Button(janela, text='Ferramentas', command=ferramentas, font=dsn['fonte2'], bg=dsn['laranja1'], fg=dsn['branco2'], relief=dsn['relif'])
-b_ferramentas.grid(column=1,row=2)
+b_ferramentas.grid(column=1,row=2,pady=dsn['y_dist'])
 
 b_tecnicos = Button(janela,text='Técnicos',command=tecnicos, font=dsn['fonte2'], bg=dsn['laranja1'], fg=dsn['branco2'], relief=dsn['relif'])
-b_tecnicos.grid(column=2,row=2)
+b_tecnicos.grid(column=2,row=2,pady=dsn['y_dist'])
 
 b_c_tecnicos = Button(janela,text=' Cadastar Técnico',command=cadastro_tecnico, font=dsn['fonte3'], bg=dsn['laranja1'], fg=dsn['branco2'], relief=dsn['relif'])
-b_c_tecnicos.grid(column=2,row=3)
+b_c_tecnicos.grid(column=2,row=3,pady=dsn['y_dist'])
 
 b_c_ferramenta = Button(janela,text=' Cadastar Ferramenta',command=casdastro_ferramenta, font=dsn['fonte3'], bg=dsn['laranja1'], fg=dsn['branco2'], relief=dsn['relif'])
 b_c_ferramenta.grid(column=1,row=3,pady=dsn['y_dist'])
